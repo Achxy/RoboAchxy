863e4fe9e5ac0bf08f0a5097db3b05d56260539f
Achxy <74144790+Achxy@users.noreply.github.com>
2022-10-05 20:10:19 +0530

    ping command

--- bot/cogs/meta.py
+++ bot/cogs/meta.py
@@ -1,5 +1,6 @@
 from bot.utils import BaseCog
 from discord.ext import commands
+import discord
 
 
 __cogs__ = ("Meta",)
@@ -9,3 +10,11 @@ class Meta(BaseCog):
     @commands.command()
     async def foo(self, ctx):
         await ctx.send("So far so good!")
+
+    @commands.command()
+    async def ping(self, ctx):
+        embed = discord.Embed(
+            title="Pong! 🏓",
+            description=f"Current Latency of the bot is {round(self.bot.latency * 1000)}ms",
+        )
+        await ctx.reply(embed=embed)
