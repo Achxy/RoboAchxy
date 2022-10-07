f8eea2a716b6871f610af974b057f9f6415dbe15
Achxy <74144790+Achxy@users.noreply.github.com>
2022-10-07 15:43:59 +0530

    use `environ`

--- bot/options.py
+++ bot/options.py
@@ -1,7 +1,14 @@
 from __future__ import annotations
 from pathlib import Path
 from json import loads
-from os import getenv
+from os import environ
+
+try:
+    import dotenv
+
+    dotenv.load_dotenv()
+except ImportError:
+    pass
 
 BOT_DIR = Path(__file__).parent
 ROOT_DIR = BOT_DIR.parent
@@ -9,12 +16,9 @@ COGS_DIR = BOT_DIR / "cogs"
 
 with (ROOT_DIR / "config.json").open("r") as cfg:
     CONFIG = loads(cfg.read())
-if (env_path := (ROOT_DIR / ".env")).exists():
-    with env_path.open("r") as env:
-        ENV = loads(env.read())
-else:
-    ENV = {}
 
 
-DISCORD_TOKEN: str = getenv("DISCORD_TOKEN") or ENV["DISCORD_TOKEN"]
+DISCORD_TOKEN: str = environ["DISCORD_TOKEN"]
+POSTGRES_DSN: str = environ["POSTGRES_DSN"]
 LOG_MAP: dict[str, dict[str, list[dict]]] = CONFIG["log_map"]
+MAX_GUILD_PREFIX_LIMIT = 10
