47037d0efb6c6cd3742d93c3a1c71fdedf74a9f8
Achxy <74144790+Achxy@users.noreply.github.com>
2022-10-08 00:04:50 +0530

    add type annotations

--- bot/_typeshack.py
+++ bot/_typeshack.py
@@ -1,6 +1,15 @@
-from typing import TYPE_CHECKING, TypeVar
+from typing import TYPE_CHECKING, Literal, TypeVar
+from collections.abc import Callable, Iterable
+
+from discord import Message
 
 if TYPE_CHECKING:
-    from .core import RoboAchxy
+    from bot.core import RoboAchxy
+    from typing_extensions import Unpack, Self
+
 else:
     RoboAchxy = TypeVar("RoboAchxy")
+    Self = TypeVar("Self")
+
+PrefixModifier = Callable[["Unpack[tuple[str, ...]]"], Callable[[RoboAchxy, Message], Iterable[str]]]
+EmptySend = Literal[None]
--- bot/tools/prefix/prefix.py
+++ bot/tools/prefix/prefix.py
@@ -1,11 +1,13 @@
+from collections.abc import Generator, Iterable
 from asyncpg import Pool
 from itertools import cycle
 from operator import itemgetter
 
 from bot.options import MAX_GUILD_PREFIX_LIMIT
-from discord.ext import commands
-from bot._typeshack import RoboAchxy
+from bot._typeshack import RoboAchxy, PrefixModifier, Self, EmptySend
 from discord import Message
+from asyncio import Future, gather
+from bot.tools.prefix._helpers import no_modifier
 
 
 class MaxGuildPrefixLimitExceeded(RuntimeError):
@@ -16,20 +18,20 @@ class MaxGuildPrefixLimitExceeded(RuntimeError):
 class Prefix:
     def __init__(
         self,
-        bot,
+        bot: RoboAchxy,
         pool: Pool,
-        modifier=commands.when_mentioned_or,
-        use_modifier_on_dm=False,
-        on_dm_return=str(),
+        modifier: PrefixModifier = no_modifier,
+        use_modifier_on_dm: bool = True,
+        on_dm_return: str = str(),
     ) -> None:
-        self.bot = bot
-        self.pool = pool
-        self.modifier = modifier
-        self.on_dm_return = on_dm_return
-        self.use_modifier_on_dm = use_modifier_on_dm
-        self._initialized = False
+        self.bot: RoboAchxy = bot
+        self.pool: Pool = pool
+        self.modifier: PrefixModifier = modifier
+        self.on_dm_return: str = on_dm_return
+        self.use_modifier_on_dm: bool = use_modifier_on_dm
+        self._initialized: bool = False
 
-    def __await__(self):
+    def __await__(self) -> Generator[Future, EmptySend, Self]:
         if self._initialized:
             return self
         coro = self.__async_init__()
@@ -37,20 +39,21 @@ class Prefix:
         yield from gener
         return self
 
-    async def __async_init__(self):
+    async def __async_init__(self) -> None:
         await self.pool
         self._initialized = True
         await self.init_table()
 
-    async def __call__(self, bot: RoboAchxy, message: Message):
-        return self.get_prefixes(bot, message)
+    async def __call__(self, bot: RoboAchxy, message: Message) -> Iterable[str]:
+        return await self.get_prefixes(bot, message)
 
-    async def init_table(self):
+    async def init_table(self) -> None:
         if not self._initialized:
             # This means that the pool has not been initialized.
             # We can await this instance and have pool initialized and table created.
             # We can return and cause termination because desired effect has then been achieved.
-            return await self
+            await self
+            return
 
         query = """
         CREATE TABLE IF NOT EXISTS guild_prefixes
@@ -70,7 +73,7 @@ class Prefix:
         """
         return await self.pool.fetchval(query, guild_id)
 
-    async def add_prefixes(self, guild_id: int, *prefixes: str, get_limit_unchecked: bool = False):
+    async def add_prefixes(self, guild_id: int, *prefixes: str, get_limit_unchecked: bool = False) -> None:
         await self
         if (
             not get_limit_unchecked
@@ -91,7 +94,7 @@ class Prefix:
         """
         await self.pool.executemany(query, zip(cycle([guild_id]), prefixes))
 
-    async def remove_prefixes(self, guild_id: int, *prefixes: str):
+    async def remove_prefixes(self, guild_id: int, *prefixes: str) -> None:
         await self
         query = """
         DELETE FROM guild_prefixes
@@ -99,7 +102,7 @@ class Prefix:
         """
         await self.pool.executemany(query, zip(cycle([guild_id]), prefixes))
 
-    async def remove_all_prefixes(self, guild_id: int):
+    async def remove_all_prefixes(self, guild_id: int) -> None:
         await self
         query = """
         DELETE FROM guild_prefixes
@@ -107,7 +110,7 @@ class Prefix:
         """
         await self.pool.execute(query, guild_id)
 
-    async def get_raw_prefixes(self, guild_id: int):
+    async def get_raw_prefixes(self, guild_id: int) -> set[str]:
         await self
         query = """
         SELECT prefix FROM guild_prefixes
@@ -116,10 +119,15 @@ class Prefix:
         result = await self.pool.fetch(query, guild_id)
         return {*map(itemgetter("prefix"), result)}
 
-    async def get_prefixes(self, bot: RoboAchxy, message: Message):
+    def _get_dm_case(self, bot, message) -> Iterable[str]:
+        empty = self.modifier()
+        if self.use_modifier_on_dm:
+            return empty(bot, message)
+        return []
+
+    async def get_prefixes(self, bot: RoboAchxy, message: Message) -> Iterable[str]:
         if message.guild is None:
-            modifier = lambda x: x if not self.use_modifier_on_dm else self.modifier
-            return modifier(self.on_dm_return)
+            return self._get_dm_case(bot, message)
         prefixes = await self.get_raw_prefixes(message.guild.id)
         modified = self.modifier(*prefixes)
         return modified(bot, message)
